9e9dd2cac75bd7a090e0dfc3c9098b1215d08f13
carlos23fb <jcflobe@gmail.com>
2020-11-13 17:36:28 -0600

    fix template and views error from newyear app

--- cs50Django/cs50Django/urls.py
+++ cs50Django/cs50Django/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('newyear', include("newyear.urls")),
+    path('newyear/', include("newyear.urls")),
     path('hello/', include("hello.urls"))
 ]
