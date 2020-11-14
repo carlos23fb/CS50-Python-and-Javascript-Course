307518027587b45980bf3a08095831efea3fe4e9
carlos23fb <jcflobe@gmail.com>
2020-11-13 18:02:34 -0600

    create newYear template

--- cs50Django/newyear/views.py
+++ cs50Django/newyear/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+import datetime
 # Create your views here.
 
 
 def index(request):
     return render(request, "newyear/index.html")
+
+
+def newYear(request):
+    now = datetime.datetime.now()
+    return render(request, "newyear/newYear.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
