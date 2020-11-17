a5b2086da750bd85355e2bfe6a8d7929f6c2eb12
carlos23fb <jcflobe@gmail.com>
2020-11-16 20:16:50 -0600

    Add inheritance layout, create formulary for create new tasks, create NewTaskForm, server side form validation

--- cs50Django/tasks/views.py
+++ cs50Django/tasks/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
+
+tasks = []
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=4)
 
 
 def index(request):
-    return HttpResponse('Hola, desde Task App')
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
