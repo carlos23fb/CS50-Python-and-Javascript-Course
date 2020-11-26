4d822eade1959c590972a1d30310a00539f75b7b
Carlos <jcflobe@gmail.com>
2020-11-26 16:59:51 -0700

    create order model and products per order view

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -26,7 +26,7 @@ class Passanger(models.Model):
     first = models.CharField(max_length=64)
     last = models.CharField(max_length=64)
     flights = models.ManyToManyField(
-        Flight, blank=True, related_name="passangers")
+        Flight, blank=True, elated_name="passangers")
 
     def __str__(self):
         return f"{self.first} {self.last}"
--- airline/users/views.py
+++ airline/users/views.py
@@ -30,6 +30,6 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return render(request, "users/login.html",{
+    return render(request, "users/login.html", {
         "message": "Logged Out."
     })
