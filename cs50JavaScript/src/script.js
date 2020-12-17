35169e8a9ea7848884a7aa6a7dda04e3cf9bc889
Carlos <jcflobe@gmail.com>
2020-12-16 20:00:54 -0700

    add event listeners

--- cs50JavaScript/src/script.js
+++ cs50JavaScript/src/script.js
@@ -1,16 +1,28 @@
-function hello(){
-    alert('Hello onClick')
+function hello() {
+	alert('Hello onClick')
 }
 
 
 let counter = 0;
 
-function count(){
-    counter += 1;
-    document.querySelector('h2').innerHTML = `Counter ${counter}`;
-    
+function count() {
+	counter += 1;
+	document.querySelector('h2').innerHTML = `Counter ${counter}`;
+
 }
 
-function heading(){
-    document.querySelector('h1').innerHTML = `Change`;
+function heading() {
+	document.querySelector('h1').innerHTML = `Change`;
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('button').onclick = count;
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('form').onsubmit = function(){
+		const name = document.querySelector('#name').value;
+		alert(name)
+	};
+})
