a53120524714beeee9e8ba1e78ddf5aca2b19bb1
Carlos <jcflobe@gmail.com>
2020-12-16 20:57:13 -0700

    using arrow functions

--- cs50JavaScript/src/script.js
+++ cs50JavaScript/src/script.js
@@ -21,8 +21,35 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 document.addEventListener('DOMContentLoaded', function () {
-	document.querySelector('form').onsubmit = function(){
+	document.querySelector('form').onsubmit = function () {
 		const name = document.querySelector('#name').value;
 		alert(name)
 	};
+})
+
+
+// * Change font color to red
+document.addEventListener('DOMContentLoaded', () => {
+
+	// document.querySelector('#red').onclick = function () {
+	// 	document.querySelector('#hello').style.color = 'red';
+	// }
+
+	// document.querySelector('#blue').onclick = function () {
+	// 	document.querySelector('#hello').style.color = 'blue';
+	// }
+
+	// document.querySelector('#green').onclick = function () {
+	// 	document.querySelector('#hello').style.color = 'green';
+	// }
+
+
+	document.querySelectorAll('#button').forEach(input => {
+		input.onclick = () =>{
+			document.querySelector('#hello').style.color = input.dataset.color;
+		}
+	}) 
+});
+document.addEventListener('DOMContentLoaded', () => {
+	
 })
